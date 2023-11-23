ef9fad71f02e66f9000b5ac87398a740211a09d4
jonathandagoberto <jonathandagoberto1@gmail.com>
2023-11-23 17:08:28 -0500

    Agregué nuevas funcionalidades 15

--- controladores/procesar_pedido.php
+++ controladores/procesar_pedido.php
@@ -2,13 +2,15 @@
 // Incluye el archivo de conexión utilizando la ruta relativa
 include(__DIR__ . '/../configuracion/conexion.php');
 
-if (isset($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] == "POST") {
-
+if (isset($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] == "POST" || True) {
     // Recibir datos del formulario
     $id_producto = $_POST['id_producto'];
     $mesa = $_POST['mesa'];
     $sede = $_POST['sede'];
     $cantidad = $_POST['cantidad'];
+
+    // Imprimir el ID recibido
+    echo "ID del producto enviado: $id_producto";
 
     // Obtener la información del producto desde la tabla de inventario
     $sql_producto = "SELECT nombre_producto, precio FROM inventario WHERE id = $id_producto";
@@ -18,14 +20,11 @@ if (isset($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] == "POST") {
         // Obtener los datos del producto
         $row_producto = $result_producto->fetch_assoc();
         $nombre_producto = $row_producto['nombre_producto'];
-        $precio_unitario = $row_producto['precio'];
-
-        // Calcular el total
-        $total = $cantidad * $precio_unitario;
+        $precio = $row_producto['precio'];
 
         // Insertar el pedido en la tabla de pedidos
-        $sql_insert_pedido = "INSERT INTO pedidos (id_producto, nombre_producto, cantidad, precio_unitario, total, mesa, sede) 
-                             VALUES ('$id_producto', '$nombre_producto', '$cantidad', '$precio_unitario', '$total', '$mesa', '$sede')";
+        $sql_insert_pedido = "INSERT INTO pedidos (id_producto, nombre_producto, cantidad, precio) 
+                             VALUES ('$id_producto', '$nombre_producto', '$cantidad', '$precio', '$total', '$mesa', '$sede')";
 
         if ($conn->query($sql_insert_pedido) === TRUE) {
             // Éxito al insertar el pedido
@@ -43,3 +42,4 @@ if (isset($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] == "POST") {
     $conn->close();
 }
 ?>
+
